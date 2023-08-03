2507f7cc387fa7cc57177de000c40089b7f5c778
Yana-Gupta <yanagupta897@gmail.com>
2023-08-03 21:34:40 +0530

    saving work of color generating

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,39 +1,56 @@
 import { useEffect, useState } from "react";
 import html2canvas from "html2canvas";
 import { useLocation } from "react-router-dom";
-
+import { getColor, getUpperWord } from "../helper/getcolor";
 
 const ConvertToImage = () => {
 
-  const [imageDataURL, setImageDataURL] = useState<string | null>(null);
+  const [imageDataURL, setImageDataURL] = useState<string>('');
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
 
 
+  const name: string = queryParams.get('name') as string
+  const color: string = queryParams.get('color') as string
+
+  const [bgColor, setBgColor] = useState<Array<string>>([])
 
   useEffect(() => {
-    const divElement = document.getElementById("generator")
 
-    if (divElement) {
-      html2canvas(divElement)
-        .then(function (canvas) {
-          const dataURL = canvas.toDataURL();
+    const convertHTMLToImg = async () => {
+      const divElement: HTMLDivElement = document.getElementById("generator") as HTMLDivElement
+      var backgroundColors: Array<string> = await getColor(); 
 
-          if (dataURL != "data:,") {
-            console.log("Hello world")
-            setImageDataURL(dataURL);
-          }
-        })
-        .catch(error =>
-          console.error(error)
-        )
+      if (backgroundColors.length > 0) {
+        setBgColor(backgroundColors)
+        console.log("from canvas")
+        console.log(backgroundColors)
+        divElement.style.backgroundColor = bgColor[0]
+        if (divElement) {
+
+          html2canvas(divElement)
+            .then(function (canvas) {
+              const dataURL = canvas.toDataURL();
+              if (dataURL != "data:,") {
+                setImageDataURL(dataURL);
+              }
+            })
+            .catch((error: Error) => {
+              console.error(error)
+            })
+
+        }
+        divElement.style.setProperty("display", "none")
+
+      }
     }
-    divElement?.style.setProperty("display", "none")
+
+    convertHTMLToImg()
+
   }, []);
 
 
   return (
-
     <div>
       <div id="generator"
         style={{
@@ -41,7 +58,8 @@ const ConvertToImage = () => {
           width: "200px",
           borderRadius: "9999px",
           backgroundColor: "transparent",
-          background: `linear-gradient(to right, #ff8177 0%, #ff867a 10%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%)`,
+          // background: `linear-gradient(to right, #ff8177 0%, #ff867a 10%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%)`,
+          // backgroundColor: bgColor[0],
           display: "flex",
           alignItems: "center",
           textAlign: "center",
@@ -49,17 +67,15 @@ const ConvertToImage = () => {
           justifyContent: "center",
           fontWeight: 600,
           fontSize: "40px",
+          color: `${color && color}`
         }}>
-        {/* Your content inside the hidden div */}
         <p>
-          Y G
+          {name && getUpperWord(name)}
         </p>
       </div>
-      {/* Display the image to users */}
       {imageDataURL && <img src={imageDataURL} alt="Captured content" />}
     </div>
   );
 };
 
 export default ConvertToImage;
-
---
+++ src/helper/getcolor.tsx
@@ -0,0 +1,34 @@
+
+const numOfColors = 6;
+
+function generateRandomHexColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+export const getColor = async (): Promise<Array<string>> => {
+    var color: Array<string> = []
+    for (var i = 0; i < numOfColors; i++) {
+        color.push(generateRandomHexColor())
+    }
+    console.log("from ", color)
+    return color
+}
+
+
+export const getUpperWord = (name: string): string => {
+    let uw: Array<string>
+    uw = name.split(' ').map((word) => {
+        return word[0]
+    })
+    let upperword: string = '';
+    for (let i = 0; i < uw.length; i++) {
+        upperword += uw[i] + ' '
+    }
+    return upperword
+}
